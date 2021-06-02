29349742796be545463de37caa498817af08b022
pgit97 <gupt.priti97@gmail.com>
2021-06-02 18:27:53 +0530

    first commit

---
+++ Q2.java
@@ -0,0 +1,23 @@
+//Q2.
+import java.util.*;
+class Q2{
+    public static void main(String args[]){
+        int A[]={1,1,1,0,0,1,1,1,1,1,1,0,0};
+        int temp=0;
+        int result=0;
+        // System.out.println("Even number are: ");
+        for(int i=0;i<A.length;i++){
+            if(A[i]==0){
+                temp=0;
+            }
+            else{
+                temp++;
+            }
+            if(temp>result){  
+                result=temp;
+                }
+
+        }
+        System.out.println(result);
+    }
+}
---
+++ Q3.java
@@ -0,0 +1,24 @@
+//Q3.
+import java.util.*;
+class Q3{
+    public static void main(String args[]){
+
+        System.out.println("Enter the length of array: ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] A=new int[n];
+        System.out.println("Enter any random number: ");
+        for(int i=0;i<A.length;i++){
+          A[i] =sc.nextInt(); 
+        }
+        //    int A[]={10,2,9,8,66,5};
+            System.out.println("Even number are: ");
+            for(int i=0;i<A.length;i++){
+                for(int j=i+1;j<A.length;j++){
+                if(A[i]==A[j]){
+                    System.out.println(A[i]);
+                }
+            }
+        }
+    }
+}
